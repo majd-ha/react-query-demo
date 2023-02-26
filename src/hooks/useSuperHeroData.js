6c1f9fd19288dc0dc4c12e0b9ac97447df8d35d6
majd-ha <zienjm@gmail.com>
2023-02-26 21:24:49 +0200

    testing

--- src/hooks/useSuperHeroData.js
+++ src/hooks/useSuperHeroData.js
@@ -5,7 +5,7 @@ export const useSuperHeroData = (onError, onSuccess) => {
   const fetchSuper = () => {
     return axios.get("http://localhost:4000/superheros");
   };
-  // eslint-disable-next-line no-unused-vars
+
   return useQuery("superhero-query", fetchSuper, {
     cacheTime: 5000,
     onError,
